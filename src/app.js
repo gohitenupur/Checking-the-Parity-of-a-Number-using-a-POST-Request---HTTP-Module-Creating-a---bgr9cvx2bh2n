9bfa22132cffe6fec712857d7921ae9d870f88c8
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-04-01 14:06:55 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const chunks = [];
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       const buf = Buffer.from(chunk);
       const str = buf.toString();
       chunks.push(str);
-      const obj = JSON.parse(chunks)
+      const obj = JSON.parse(chunks);
       const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
 
-   });
+      // Write the code here to check if the number is odd or even
+      if (value % 2 === 0) {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is even`);
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end(`The number ${value} is odd`);
+      }
+    });
   }
-
-  
 });
-
 
 module.exports = server;
